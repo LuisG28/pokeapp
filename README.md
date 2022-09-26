5ea453c4bd6c2b231e38965f431266de63c68867
LuisG28 <luis.gus2869@gmail.com>
2022-09-25 22:34:15 -0500

    adding loader and changing text  readme

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
+why Next.js?
+    For this practice I use next js because it helps me to 
+    create react js applications quickly including navigation paths
+
 Commands to run this app 
-    npm i //to install dependencies
-    npm run dev // to run the app
+    First run this command : npm i
+    Wait for dependencies to be installed
+    To initialize the app run the command : npm run dev
+    open url displayed in console for example : http://localhost:3000
 
     
--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,20 @@ import React, { useEffect, useState } from 'react';
 import { getPokemon } from '../pages/api/pokemon';
 import { colorsStyle } from '../utils/colors';
 import CardList from '../components/cardList';
+import {
+  Col,
+} from 'react-bootstrap';
 
 export default function Home() {
   //state for save result
   const [pokemon, setpokemon] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
   const listPokemon = async(params = {}) => {
     //get results of pokemon
     const oResult = await getPokemon(params);
     setpokemon(oResult);
+    setIsLoading(false);
   }
 
   useEffect(() => {
@@ -19,6 +24,7 @@ export default function Home() {
 
 
   const pagination = (sParam) => {
+    setIsLoading(true);
     let params = {
       url : sParam === 'next' ? pokemon.next : pokemon.previous
     }
@@ -27,6 +33,7 @@ export default function Home() {
 
   //build filter for search pokemon
   const filterPokemon = (sFilter) => {
+    setIsLoading(true);
     let params = {
       nameOrId : sFilter
     }
@@ -35,55 +42,65 @@ export default function Home() {
   
   return (
     <div className="container">
-      <div className="row d-flex justify-content-center mt-4 mb-4">
-        <div className="col-lg-6">
-          <input 
-            type="text" 
-            className="form-control" 
-            placeholder="Bucar por nombre o número"
-            onChange={( { target } )=>{
-              filterPokemon(target.value);
-            }}
-          />
-        </div>
-      </div>
-      <div className="row d-flex justify-content-center">
-        {
-          pokemon && (
-            pokemon.pokemon.map((oPokemon, nIndex) => {
-              let oColor = colorsStyle.find(color => color.type === oPokemon.type)
-              return (
-                <CardList
-                  item={oPokemon}
-                  color={oColor}
-                  nIndex={nIndex}
-                  key={nIndex}
+      {
+        isLoading ? (
+          <div className="d-flex justify-content-center" style={{height: '50rem'}}>
+            <img className="align-self-center" src="../pokeball.gif"  width="200" height="200"/>
+          </div>
+        ) : (
+          <> 
+            <div className="row d-flex justify-content-center mt-4 mb-4">
+              <div className="col-lg-6">
+                <input 
+                  type="text" 
+                  className="form-control" 
+                  placeholder="Bucar por nombre o número"
+                  onChange={( { target } )=>{
+                    filterPokemon(target.value);
+                  }}
                 />
-              )
-            })
-          )
-        }
-        {
-          pokemon && pokemon?.pokemon.length > 1 && (
-            <div className="d-flex justify-content-center">
-              <nav aria-label="Page navigation example">
-                <ul className="pagination">
-                  <li className="page-item">
-                    <a className="page-link" href="#" aria-label="Previous" onClick={()=>{pagination('prev')}}>
-                      <span aria-hidden="true">&laquo;</span>
-                    </a>
-                  </li>
-                  <li className="page-item">
-                    <a className="page-link" href="#" aria-label="Next" onClick={()=>{pagination('next')}}>
-                      <span aria-hidden="true">&raquo;</span>
-                    </a>
-                  </li>
-                </ul>
-              </nav>
+              </div>
             </div>
-          )
-        }
-      </div>
+            <div className="row d-flex justify-content-center">
+              {
+                pokemon && (
+                  pokemon.pokemon.map((oPokemon, nIndex) => {
+                    let oColor = colorsStyle.find(color => color.type === oPokemon.type)
+                    return (
+                      <CardList
+                        item={oPokemon}
+                        color={oColor}
+                        nIndex={nIndex}
+                        key={nIndex}
+                      />
+                    )
+                  })
+                )
+              }
+              {
+                pokemon && pokemon?.pokemon.length > 1 && (
+                  <div className="d-flex justify-content-center">
+                    <nav aria-label="Page navigation example">
+                      <ul className="pagination">
+                        <li className="page-item">
+                          <a className="page-link" href="#" aria-label="Previous" onClick={()=>{pagination('prev')}}>
+                            <span aria-hidden="true">&laquo;</span>
+                          </a>
+                        </li>
+                        <li className="page-item">
+                          <a className="page-link" href="#" aria-label="Next" onClick={()=>{pagination('next')}}>
+                            <span aria-hidden="true">&raquo;</span>
+                          </a>
+                        </li>
+                      </ul>
+                    </nav>
+                  </div>
+                )
+              }
+            </div>
+          </>
+        )
+      }
     </div>
   )
 }
